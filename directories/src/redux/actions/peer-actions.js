14598e845e2235d3a6b3b26b56468087e2fa4f6d
Ladislav Balon <tenisakb@seznam.cz>
2018-03-01 18:09:15 +0100

    API improve, cleaning...

--- directories/src/redux/actions/peer-actions.js
+++ directories/src/redux/actions/peer-actions.js
@@ -1,46 +1,31 @@
-const PEER_INIT = 'directories/peer/init';
-const PEER_CONNECT_TO = 'directories/peer/connect/to';
-const PEER_SEND_MESSAGE = 'directories/peer/send/message';
-const PEER_OPEN = 'directories/peer/open';
-const PEER_CONNECTION = 'directories/peer/connection';
-const PEER_CONNECTING = 'directories/peer/connecting';
+import {
+  PEER_RECEIVE_REQUEST,
+  PEER_RECIEVE_SUCCESS,
+  PEER_SEND_REQUEST,
+  PEER_SEND_SUCCESS
+} from './action-types';
 
-const ACTIONS = Object.freeze({
-  // actions
-  PEER_INIT,
-  PEER_CONNECT_TO,
-  PEER_SEND_MESSAGE,
-  PEER_OPEN,
-  PEER_CONNECTION,
-  PEER_CONNECTING,
-
-  // actionCreators
-  peerInitAction: () => ({
-    type: ACTIONS.PEER_INIT
-  }),
-
-  peerConnectToAction: (remotePeerId) => ({
-    type: ACTIONS.PEER_CONNECT_TO,
-    remotePeerId: remotePeerId
-  }),
-
-  peerSendMessageAction: (message) => ({
-    type: ACTIONS.PEER_SEND_MESSAGE,
-    message: message
-  }),
-
-  peerOpenAction: () => ({
-    type: ACTIONS.PEER_OPEN
-  }),
-
-  peerConnectionAction: () => ({
-    type: ACTIONS.PEER_CONNECTION
-  }),
-
-  peerConnectingAction: () => ({
-    type: ACTIONS.PEER_CONNECTING
-  })
-
+export const requestPeerReceive = peer => ({
+  type: PEER_RECEIVE_REQUEST,
+  peer
 });
 
-export default ACTIONS;
+export const fullfillPeerReceive = payload => ({
+  type: PEER_RECIEVE_SUCCESS,
+  payload
+});
+
+export const requestPeerSend = (peer, receiver, message) => ({
+  type: PEER_SEND_REQUEST,
+  peer,
+  payload: {
+    message,
+    receiver,
+    sender: peer.id
+  }
+});
+
+export const fullfillPeerSend = payload => ({
+  type: PEER_SEND_SUCCESS,
+  payload
+});
