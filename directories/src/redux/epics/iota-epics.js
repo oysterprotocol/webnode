3f649dca08713db34744536483994e77ba1c1136
Ladislav Balon <tenisakb@seznam.cz>
2018-03-08 13:26:45 +0100

    IOTA PoW

--- directories/src/redux/epics/iota-epics.js
+++ directories/src/redux/epics/iota-epics.js
@@ -1,40 +1,16 @@
-import { combineEpics } from 'redux-observable';
 import { Observable } from 'rxjs';
+import { combineEpics } from 'redux-observable';
 import { IOTA_PREPARE_TRANSFERS } from '../actions/action-types';
 import { fullfillPrepareTranfers } from '../actions/iota-actions';
-
-import IOTA from "iota.lib.js";
-import { IOTA_API_PROVIDER } from "../../config";
-
-const providerIota = new IOTA({
-  provider: IOTA_API_PROVIDER
-});
+import { prepareTransfers } from '../../services/iota';
 
 const prepareTransfersEpics = (action$, store) => {
   return action$.ofType(IOTA_PREPARE_TRANSFERS).mergeMap(action => {
     const { data } = action.payload;
-    return Observable.fromPromise(
-      new Promise((resolve, reject) => {
-        providerIota.api.prepareTransfers(
-          data.seed,
-          [{
-            'address': data.address,
-            'value': data.value,
-            'message': data.message,
-            'tag': data.tag
-          }],
-          (error, prepareTransfers) => {
-            if(error) {
-              console.log("IOTA ERROR: ", error);
-            } else {
-              resolve(prepareTransfers);
-            }
-          }
-        )
-      })
-    ).map(prepareTransfers =>
-      fullfillPrepareTranfers({ prepareTransfers })
-    ).catch(error => Observable.empty())
+    return Observable
+      .fromPromise(prepareTransfers(data))
+      .map(prepareTransfers => fullfillPrepareTranfers({ prepareTransfers }))
+      .catch(error => Observable.empty())
   });
 };
 
