eaf94db7e314507b5da56dea009845cad3a73290
tenisakb <7314503+tenisakb@users.noreply.github.com>
2018-03-14 14:20:09 +0100

    api reducer fix (#12)

--- directories/src/redux/reducers/items-reducers.js
+++ directories/src/redux/reducers/items-reducers.js
@@ -12,22 +12,25 @@ const initState = {
 };
 
 export default (state = initState, action) => {
-  const payload = action.payload;
-  if (payload && payload.error === API_REQUEST_ERROR) {
-    action.type = API_REQUEST_ERROR;
+  let payload = null;
+  if(action.payload) {
+    payload = action.payload;
   }
+  if(payload !== null) {
+    switch (action.type) {
+      case API_START_TRANSACTION:
+        return {
+           ...state,
+          transaction: { txId: payload.data.txtid, items: payload.data.items }
+        };
+        
+      case API_SELECT_NEED:
+        return { ...state, item: payload.data.data };
 
-  switch (action.type) {
-    case API_START_TRANSACTION:
-      return {
-        ...state,
-        transaction: { txId: payload.txtid, items: payload.items }
-      };
-
-    case API_SELECT_NEED:
-      return { ...state, item: action.payload.data };
-
-    default:
-      return state;
+      default:
+        return state;
+    }
   }
+  console.log('here');
+  return state;
 };
