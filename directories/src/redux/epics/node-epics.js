07c898eb1c3003d7f524cb5b0e51a55b765c070a
Edmund Mai <edmundmai@gmail.com>
2018-03-28 17:52:12 -0400

    post self id after being reset

--- directories/src/redux/epics/node-epics.js
+++ directories/src/redux/epics/node-epics.js
@@ -7,9 +7,24 @@ import brokerNode from "../services/broker-node";
 
 import { MIN_BROKER_NODES } from "../../config/";
 
+const registerWebnodeEpic = (action$, store) => {
+  return action$.ofType(nodeActions.NODE_RESET).mergeMap(action => {
+    const { id } = action.payload;
+    return Observable.fromPromise(brokerNode.registerWebnode(id))
+      .map(({ data }) => {
+        console.log("/api/v1/supply/webnodes response:", data);
+        return nodeActions.determineRequest();
+      })
+      .catch(error => {
+        console.log("/api/v1/supply/webnodes error:", error);
+        return Observable.of(nodeActions.determineRequest());
+      });
+  });
+};
+
 const findMoreWorkEpic = (action$, store) => {
   return action$
-    .ofType(nodeActions.NODE_RESET, nodeActions.NODE_ADD_BROKER_NODE)
+    .ofType(nodeActions.NODE_ADD_BROKER_NODE)
     .map(nodeActions.determineRequest);
 };
 
@@ -25,19 +40,23 @@ const determineRequestEpic = (action$, store) => {
 
 const requestBrokerEpic = (action$, store) => {
   return action$.ofType(nodeActions.NODE_REQUEST_BROKER_NODES).mergeMap(() => {
-    return Observable.fromPromise(brokerNode.requestBrokerNodeAddresses())
+    const { brokerNodes } = store.getState().node;
+    return Observable.fromPromise(
+      brokerNode.requestBrokerNodeAddresses(brokerNodes)
+    )
       .map(({ data }) => {
-        console.log("/api/v1/broker_nodes response:", data);
+        console.log("/api/v1/supply/webnodes response:", data);
         return nodeActions.addBrokerNode("123.123.123");
       })
       .catch(error => {
-        console.log("/api/v1/broker_nodes error:", error);
+        console.log("/api/v1/supply/webnodes error:", error);
         return Observable.of(nodeActions.addBrokerNode("123.123.123"));
       });
   });
 };
 
 export default combineEpics(
+  registerWebnodeEpic,
   findMoreWorkEpic,
   determineRequestEpic,
   requestBrokerEpic
--- directories/src/redux/services/broker-node.js
+++ directories/src/redux/services/broker-node.js
@@ -1,15 +1,27 @@
 import axios from "axios";
 import { API_HEADERS, API_ROOT_URL, API_REQUEST_ERROR } from "../../config/";
 
-const requestBrokerNodeAddresses = () =>
+const registerWebnode = address =>
   axios({
-    method: "GET",
-    url: `${API_ROOT_URL}/api/v1/broker_nodes`
+    method: "POST",
+    url: `${API_ROOT_URL}/api/v1/supply/webnodes`,
+    data: { address }
+  }).catch(error => {
+    console.log("POST " + error);
+    return { error: API_REQUEST_ERROR };
+  });
+
+const requestBrokerNodeAddresses = currentList =>
+  axios({
+    method: "POST",
+    url: `${API_ROOT_URL}/api/v1/demand/transactions/brokernodes`,
+    data: { currentList }
   }).catch(error => {
     console.log("PUT " + error);
     return { error: API_REQUEST_ERROR };
   });
 
 export default {
+  registerWebnode,
   requestBrokerNodeAddresses
 };
