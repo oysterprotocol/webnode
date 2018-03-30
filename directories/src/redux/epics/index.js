06dc5c914ebb1e2d02c6b5731e03cc9d9065c51d
Edmund Mai <edmundmai@gmail.com>
2018-03-30 11:03:17 -0400

    remove unused legacy stuff

--- directories/src/redux/epics/index.js
+++ directories/src/redux/epics/index.js
@@ -1,17 +1,6 @@
 import "rxjs";
 import { combineEpics } from "redux-observable";
 import appEpics from "./app-epics";
-import peerEpics from "./peer-epics";
 import nodeEpics from "./node-epics";
-import iotaEpics from "./iota-epics";
-import powEpics from "./pow-epics";
-import itemsEpic from "./items-epics";
 
-export default combineEpics(
-  appEpics,
-  peerEpics,
-  itemsEpic,
-  nodeEpics,
-  iotaEpics,
-  powEpics
-);
+export default combineEpics(appEpics, nodeEpics);
--- directories/src/redux/reducers/index.js
+++ directories/src/redux/reducers/index.js
@@ -1,10 +1,5 @@
 import { combineReducers } from "redux";
 
-import storage from "redux/reducers/storage-reducers";
-import peer from "redux/reducers/peer-reducers";
-import items from "redux/reducers/items-reducers";
-import iota from "redux/reducers/iota-reducers";
-import pow from "redux/reducers/pow-reducers";
 import node from "redux/reducers/node-reducer";
 
-export default combineReducers({ storage, peer, items, iota, pow, node });
+export default combineReducers({ node });
