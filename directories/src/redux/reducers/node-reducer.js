9d6eae330d3f5f6c37954ebab6293b539c56860d
Edmund Mai <edmundmai@gmail.com>
2018-04-15 11:04:51 -0400

    reducer for genesis hashes

--- directories/src/redux/reducers/node-reducer.js
+++ directories/src/redux/reducers/node-reducer.js
@@ -1,9 +1,14 @@
 import _ from "lodash";
 
-import { NODE_ADD_BROKER_NODE, NODE_RESET } from "../actions/node-actions";
+import {
+  NODE_ADD_BROKER_NODE,
+  NODE_ADD_GENESIS_HASH,
+  NODE_RESET
+} from "../actions/node-actions";
 
 const initState = {
   brokerNodes: [],
+  genesisHashes: [],
   id: null,
   lastResetAt: new Date()
 };
@@ -16,9 +21,15 @@ export default (state = initState, action) => {
         brokerNodes: _.uniq([...state.brokerNodes, action.payload])
       };
 
+    case NODE_ADD_GENESIS_HASH:
+      return {
+        ...state,
+        genesisHashes: _.uniq([...state.genesisHashes, action.payload])
+      };
+
     case NODE_RESET:
       const { id, lastResetAt } = action.payload;
-      return { ...state, id, lastResetAt, brokerNodes: [], webNodes: [] };
+      return { ...state, id, lastResetAt, brokerNodes: [], genesisHashes: [] };
 
     default:
       return { ...state };
