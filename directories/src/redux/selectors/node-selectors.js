582222e26f91aa934c186ec81e7f74534e1d7928
Edmund Mai <edmundmai@gmail.com>
2018-04-19 12:15:33 -0400

    add reselect

---
+++ directories/src/redux/selectors/node-selectors.js
@@ -0,0 +1,41 @@
+import { createSelector } from "reselect";
+import _ from "lodash";
+
+import { SECTOR_STATUS } from "../../config/";
+const { NOT_STARTED, SEARCHING, TREASURE_FOUND } = SECTOR_STATUS;
+
+const getNewGenesisHashes = state => state.node.newGenesisHashes;
+
+const treasureHuntableGenesisHash = createSelector(
+  [getNewGenesisHashes],
+  newGenesisHashes => {
+    return _.find(newGenesisHashes, gh => {
+      return _.find(gh.sectors, sector =>
+        _.includes([NOT_STARTED, SEARCHING, TREASURE_FOUND], sector.status)
+      );
+    });
+  }
+);
+
+const treasureHuntableSector = createSelector(
+  [treasureHuntableGenesisHash],
+  genesisHash => {
+    return _.find(
+      genesisHash.sectors,
+      sector =>
+        sector.status === TREASURE_FOUND ||
+        sector.status === SEARCHING ||
+        sector.status === NOT_STARTED
+    );
+  }
+);
+
+const unclaimedGenesisHashExists = createSelector(
+  [treasureHuntableGenesisHash],
+  genesisHash => !!genesisHash
+);
+
+export default {
+  unclaimedGenesisHashExists,
+  treasureHuntableSector
+};
