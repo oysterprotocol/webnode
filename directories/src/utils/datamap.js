8b489ebec5c9210bbfb38c9a4f7e864b28c1f4ae
Edmund Mai <edmundmai@gmail.com>
2018-05-07 15:36:10 -0400

    remove

--- directories/src/utils/datamap.js
+++ directories/src/utils/datamap.js
@@ -2,23 +2,6 @@ import _ from "lodash";
 import iota from "redux/services/iota";
 import Encryption from "utils/encryption";
 
-// const generate = (handle, size) => {
-// const keys = _.range(0, size + 1);
-
-// const [dataMap, _hash] = _.reduce(
-// keys,
-// ([dataM, hash], i) => {
-// const [obfuscatedHash, nextHash] = Encryption.hashChain(hash);
-// dataM[i] = iota.toAddress(iota.utils.toTrytes(obfuscatedHash));
-
-// return [dataM, nextHash];
-// },
-// [{}, Encryption.genesisHash(handle)]
-// );
-
-// return dataMap;
-// };
-
 const rawGenerate = (handle, size) => {
   const keys = _.range(0, size + 1);
 
