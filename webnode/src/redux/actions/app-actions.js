325a0d22846eeb1a962c5e1426d72b7e9785d5a2
Ali <kalisami@yahoo.de>
2018-05-23 20:40:07 +0200

    Added consent overlay to redux state management (#76)
    * creation of actions and reducer cases for overlay functionality
    
    * implemented functionality for dismissal of overlay and state management

--- webnode/src/redux/actions/app-actions.js
+++ webnode/src/redux/actions/app-actions.js
@@ -1,12 +1,24 @@
 export const APP_START = "directories/app/start";
 
+export const CONSENT_GIVEN = "consentGiven";
+
+export const CONSENT_DENIED = "consentDenied";
+
 const ACTIONS = Object.freeze({
   // actions
   APP_START,
+  CONSENT_GIVEN,
+  CONSENT_DENIED,
 
   // actionCreators
   startApp: () => ({
     type: APP_START
+  }),
+  consentGiven: () => ({
+    type: CONSENT_GIVEN
+  }),
+  consentDenied: () => ({
+    type: CONSENT_DENIED
   })
 });
 
