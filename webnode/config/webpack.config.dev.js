d973db348b9daf50ee23dd20aadcd4dd7a0ed06c
Jared Farago <jared.farago@gmail.com>
2018-05-26 12:42:31 -0400

    Moved plugins and presets to .babelrc   - Updated start script to be development mode   - Commented out worker loader for now

--- webnode/config/webpack.config.dev.js
+++ webnode/config/webpack.config.dev.js
@@ -190,10 +190,10 @@ module.exports = {
               },
             ],
           },
-          {
+          /*{
             test: /worker\.js$/,
             use: { loader: 'worker-loader' }
-          },
+          },*/
           // "file" loader makes sure those assets get served by WebpackDevServer.
           // When you `import` an asset, you get its (virtual) filename.
           // In production, they would get copied to the `build` folder.
