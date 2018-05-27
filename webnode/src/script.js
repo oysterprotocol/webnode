860968dd5307543838287dddf7638f818e80f200
Edmund Mai <edmundmai@gmail.com>
2018-05-27 18:48:37 -0400

    script for starting app -- wip

---
+++ webnode/src/script.js
@@ -0,0 +1,11 @@
+import { store } from "./redux";
+import appActions from "./redux/actions/app-actions";
+
+// exports.start = () => {
+// console.log("when loaded as an npm module and called");
+// };
+
+if (!module.parent) {
+  console.log("when loaded as a script");
+  store.dispatch(appActions.startApp());
+}
