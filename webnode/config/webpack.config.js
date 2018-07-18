24000931d687907daba3fa3fef4556067b93d037
Edmund Mai <edmundmai@gmail.com>
2018-07-18 11:25:08 -0400

    closer

--- webnode/config/webpack.config.js
+++ webnode/config/webpack.config.js
@@ -13,7 +13,7 @@ const eslintFormatter = require("react-dev-utils/eslintFormatter");
 const ModuleScopePlugin = require("react-dev-utils/ModuleScopePlugin");
 const MinifyPlugin = require("babel-minify-webpack-plugin");
 const ScriptExtHtmlWebpackPlugin = require("script-ext-html-webpack-plugin");
-const { CheckerPlugin } = require('awesome-typescript-loader')
+const { CheckerPlugin } = require("awesome-typescript-loader");
 
 const autoprefixer = require("autoprefixer");
 const path = require("path");
@@ -34,6 +34,11 @@ const common = {
   module: {
     strictExportPresence: true,
     rules: [
+      {
+        test: /\.tsx?$/,
+        include: paths.appSrc,
+        loader: "awesome-typescript-loader"
+      },
       {
         test: /\.(js|jsx|mjs)$/,
         enforce: "pre",
@@ -62,11 +67,6 @@ const common = {
               limit: 10000,
               name: "static/media/[name].[ext]"
             }
-          },
-          {
-            test: /\.tsx?$/,
-            include: paths.appSrc,
-            loader: require.resolve('awesome-typescript-loader')
           },
           // Process JS with Babel.
           {
@@ -135,12 +135,20 @@ if (env.stringified["process.env"].NODE_ENV === '"production"') {
       modules: ["node_modules", paths.appNodeModules].concat(
         process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
       ),
-      extensions: [".web.js", ".mjs", ".js", ".json", ".web.jsx", ".jsx", '.tsx', '.ts'],
-      mainFields: ['module', 'browser', 'main'],
+      extensions: [
+        ".web.js",
+        ".mjs",
+        ".js",
+        ".json",
+        ".web.jsx",
+        ".jsx",
+        ".tsx",
+        ".ts"
+      ],
+      mainFields: ["module", "browser", "main"],
       plugins: [new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson])]
     },
     plugins: [
-      new CheckerPlugin(),
       new BundleAnalyzerPlugin({
         generateStatsFile: generateStatsFile
       }),
@@ -221,7 +229,25 @@ if (env.stringified["process.env"].NODE_ENV === '"development"') {
       filename: "static/js/[name].bundle.[hash:8].js",
       chunkFilename: "static/js/[name].chunk.[chunkhash:8].js"
     },
+    resolve: {
+      modules: ["node_modules", paths.appNodeModules].concat(
+        process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
+      ),
+      extensions: [
+        ".web.js",
+        ".mjs",
+        ".js",
+        ".json",
+        ".web.jsx",
+        ".jsx",
+        ".tsx",
+        ".ts"
+      ],
+      mainFields: ["module", "browser", "main"],
+      plugins: [new ModuleScopePlugin(paths.appSrc, [paths.appPackageJson])]
+    },
     plugins: [
+      new CheckerPlugin(),
       new HtmlWebpackPlugin({
         inject: true,
         template: paths.appHtml
