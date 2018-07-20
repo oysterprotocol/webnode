0abd6e708a0309c311fe063628ebb17d1163f000
Edmund Mai <edmundmai@gmail.com>
2018-07-20 14:53:08 -0400

    convert buttons to styled components

--- webnode/src/redux/index.js
+++ webnode/src/redux/index.js
@@ -24,7 +24,7 @@ const storeEnhancer = [applyMiddleware(...middleware)];
 const persistConfig = {
   key: "oyster-webnode",
   storage,
-  whitelist: DEBUGGING ? [] : ["consent", "node", "treasureHunt"]
+  whitelist: true ? [] : ["consent", "node", "treasureHunt"]
 };
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
