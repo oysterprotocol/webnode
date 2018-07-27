56449b860cef1930c0a1cadb9d6e70698773c08f
Edmund Mai <edmundmai@gmail.com>
2018-07-27 11:37:13 -0400

    point to QA broker

--- webnode/src/config/index.tsx
+++ webnode/src/config/index.tsx
@@ -2,10 +2,7 @@ export const IS_DEV = process.env.NODE_ENV === "development";
 export const DEBUGGING = process.env.DEBUG;
 export const API_VERSION = "api/v2";
 
-export const API_ROOT_URL =
-  window.location && window.location.href.indexOf("localhost") > 0
-    ? "http://18.217.133.146:3000"
-    : "https://broker-1.oysternodes.com:3000";
+export const API_ROOT_URL = "http://18.217.133.146:3000";
 
 export const ASSET_URL =
   window.location && window.location.href.indexOf("localhost") > 0
